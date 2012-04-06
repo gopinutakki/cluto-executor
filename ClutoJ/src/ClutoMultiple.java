2889fad99a441df81a1fb8b04e6ea32726317676
gopi.nutakki@gmail.com <gopi.nutakki@gmail.com>
2012-04-06 17:15:26 -0500

    Formatted.

--- ClutoJ/src/ClutoMultiple.java
+++ ClutoJ/src/ClutoMultiple.java
@@ -12,26 +12,26 @@ public class ClutoMultiple {
 	}
 }
 
-class ClutoRun{
+class ClutoRun {
 	List<String> datasets = new ArrayList<String>();
 	List<String> clusteringMethods = new ArrayList<String>();
 	List<String> similarityMeasures = new ArrayList<String>();
 	List<String> criterionFunctions = new ArrayList<String>();
-	
+
 	static List<ClutoCmd> clutoResults = new ArrayList<ClutoCmd>();
-	
-	public ClutoRun(){
+
+	public ClutoRun() {
 		datasets.add("new3");
 		datasets.add("sports");
-		
+
 		clusteringMethods.add("rb");
 		clusteringMethods.add("direct");
 		clusteringMethods.add("agglo");
 		clusteringMethods.add("graph");
-		
+
 		similarityMeasures.add("cos");
 		similarityMeasures.add("corr");
-		
+
 		criterionFunctions.add("i1");
 		criterionFunctions.add("i2");
 		criterionFunctions.add("e1");
@@ -43,66 +43,75 @@ class ClutoRun{
 		criterionFunctions.add("wslink");
 		criterionFunctions.add("clink");
 		criterionFunctions.add("wclink");
-		criterionFunctions.add("upgma");		
+		criterionFunctions.add("upgma");
 	}
-	
-	public void clutoExecute(){
+
+	public void clutoExecute() {
 		String cmd = "vcluster sports.mat 10";
 		ExecutorService clutoService = Executors.newFixedThreadPool(1);
-		for(int d = 0; d < datasets.size(); d++)
-			for(int cm = 0; cm < clusteringMethods.size(); cm++)
-				for(int sm = 0; sm < similarityMeasures.size(); sm++)
-					for(int cf = 0; cf < criterionFunctions.size(); cf++){
-						cmd = "vcluster -clmethod=" + clusteringMethods.get(cm)+ " -rclassfile=" + datasets.get(d) + ".rclass -clabelfile=" + datasets.get(d) + ".mat.clabel -nfeatures=8 -showsummaries=cliques -showfeatures -showtree -labeltree -sim=" + similarityMeasures.get(sm)+ " " + datasets.get(d) + ".mat 10" ;
+		for (int d = 0; d < datasets.size(); d++)
+			for (int cm = 0; cm < clusteringMethods.size(); cm++)
+				for (int sm = 0; sm < similarityMeasures.size(); sm++)
+					for (int cf = 0; cf < criterionFunctions.size(); cf++) {
+						cmd = "vcluster -clmethod="
+								+ clusteringMethods.get(cm)
+								+ " -rclassfile="
+								+ datasets.get(d)
+								+ ".rclass -clabelfile="
+								+ datasets.get(d)
+								+ ".mat.clabel -nfeatures=8 -showsummaries=cliques -showfeatures -showtree -labeltree -sim="
+								+ similarityMeasures.get(sm) + " "
+								+ datasets.get(d) + ".mat 10";
 						Runnable runCmd = new ClutoConcurrent(cmd);
 						clutoService.execute(runCmd);
-						//System.out.println(cmd);
+						// System.out.println(cmd);
 					}
-		
-		while(!clutoService.isTerminated()){
-			
+
+		while (!clutoService.isTerminated()) {
+
 		}
 		System.out.println("Done!");
-	}	
-	
-	public static void runClutoCommand(String cmd) throws IOException{
+	}
+
+	public static void runClutoCommand(String cmd) throws IOException {
 		Process p = Runtime.getRuntime().exec(cmd);
 		Scanner sc = new Scanner(p.getInputStream());
 		String line = "";
-		while(sc.hasNext()){
+		while (sc.hasNext()) {
 			line = sc.nextLine();
-			if(line.contains("Entropy: ") && line.contains("Purity: ")){
+			if (line.contains("Entropy: ") && line.contains("Purity: ")) {
 				clutoResults.add(new ClutoCmd(cmd));
-			}				
+			}
 		}
 	}
 }
 
-class ClutoCmd{
+class ClutoCmd {
 	String cmd = "";
 	double entropy = 0.0;
 	double purity = 0.0;
-	
-	public ClutoCmd(String command){
+
+	public ClutoCmd(String command) {
 		this.cmd = command;
-		
-		String []line = command.split(" ");
-		for(int index = 0; index < line.length; index++){
-			if(line[index].contains("Entropy:"))
-				this.entropy = Double.parseDouble(line[index+1]);
-			if(line[index].contains("Purity:"))
-				this.purity = Double.parseDouble(line[index+1]);			
-		}		
-	}	
+
+		String[] line = command.split(" ");
+		for (int index = 0; index < line.length; index++) {
+			if (line[index].contains("Entropy:"))
+				this.entropy = Double.parseDouble(line[index + 1]);
+			if (line[index].contains("Purity:"))
+				this.purity = Double.parseDouble(line[index + 1]);
+		}
+	}
 }
 
-class ClutoConcurrent implements Runnable{
+class ClutoConcurrent implements Runnable {
 
 	String cmd = "";
-	
-	public ClutoConcurrent(String command){
+
+	public ClutoConcurrent(String command) {
 		this.cmd = command;
 	}
+
 	@Override
 	public void run() {
 		try {
